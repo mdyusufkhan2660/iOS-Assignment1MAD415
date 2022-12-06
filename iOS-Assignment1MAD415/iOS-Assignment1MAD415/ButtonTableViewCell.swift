4752312879be37b41083ee038a8c339df71192e2
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-12-06 12:42:53 -0500

    minor changes

--- iOS-Assignment1MAD415/iOS-Assignment1MAD415/ButtonTableViewCell.swift
+++ iOS-Assignment1MAD415/iOS-Assignment1MAD415/ButtonTableViewCell.swift
@@ -8,16 +8,13 @@
 import UIKit
 
 class ButtonTableViewCell: UITableViewCell {
+    static let identifier = "ButtonTableViewCell"
 
+    @IBAction func myButton(_ sender: Any) {
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
