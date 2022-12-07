fa816e67dc09217f5f0d7fae095ff0296e0f7d11
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2022-12-07 08:49:13 -0500

    added segment control

--- iOS-Assignment1MAD415/iOS-Assignment1MAD415/SegmentTableViewCell.swift
+++ iOS-Assignment1MAD415/iOS-Assignment1MAD415/SegmentTableViewCell.swift
@@ -8,16 +8,29 @@
 import UIKit
 
 class SegmentTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var segmentStatus: UILabel!
+    @IBOutlet weak var mySegment: UISegmentedControl!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    @IBAction func segmentControl(_ sender: UISegmentedControl) {
+        switch mySegment.selectedSegmentIndex{
+        case 0:
+            segmentStatus.text = "First"
+        case 1:
+            segmentStatus.text = "Second"
+        default:
+            break
+        }
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        
     }
+    
     
 }
